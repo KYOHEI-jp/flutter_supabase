b764415aedc4bbe479cbdaff6ff736fff2a4777b
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-03-11 18:47:38 +0900

    start_pageに入力フォーム、画像、ボタンの追加

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -17,17 +17,47 @@ class _StartPageState extends State<StartPage> {
   final _formKey = GlobalKey<FormState>();
 
   @override
+  void dispose() {
+    supabase.dispose();
+    // メモリリークを無くす
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Start Page",
-          textAlign: TextAlign.center,
-        ),
-      ),
       body: Center(
-        child: Text("Start Page"),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    "https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png",
+                    height: 150,
+                  ),
+                  const SizedBox(height: 25),
+                  // Email Field
+                  TextFormField(),
+
+                  // Password Field
+                  TextFormField(),
+                  const SizedBox(height: 25),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Sign In"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
