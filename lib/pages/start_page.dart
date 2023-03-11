174e2d4c5fa9d3633276609eb4115c5ef105cf9f
KYOHEI-jp <kingdomhearts510@gmail.com>
2023-03-11 18:35:48 +0900

    start_pageをstatefulへ

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
   const StartPage({Key? key}) : super(key: key);
 
   @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  bool _signInLoading = false;
+  bool _signUpLoading = false;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Start Page",
+          textAlign: TextAlign.center,
+        ),
+      ),
       body: Center(
         child: Text("Start Page"),
       ),
